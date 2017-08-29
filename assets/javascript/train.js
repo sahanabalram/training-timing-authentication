c3a47b401bd00d74d2762221bcd5b539cb4aca03
Sahana Balram <sahana2008balram@gmail.com>
2017-08-29 16:31:45 -0700

    add code for train info to get displayed in html and firebase

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -17,7 +17,7 @@
 
     //   grab the user input
     var trainName = $("#add-train-input").val().trim();
-    var destinationName = $("#add-destination-input").val().trim();
+    var trainDestination = $("#add-destination-input").val().trim();
     var trainTime = $("#add-train-time-input").val().trim();
     var trainFrequency = $("#add-frequency-input").val().trim();
 
@@ -30,5 +30,38 @@
     }
 // Upload train data to the database
   database.ref().push(newTrain);
-  
-  });
+  // Log everything to console
+  console.log(newTrain.name);
+  console.log(newTrain.destination);
+  console.log(newTrain.time);
+  console.log(newTrain.frequency);
+
+//   alert train successfully added
+alert("Train successfully added");
+
+// clear all of the text-boxes
+$("#add-train-input").val("");
+$("#add-destination-input").val("");
+$("#add-train-time-input").val("");
+$("add-frequency-input").val("");
+ });
+// Create Firebase event for adding train to the database and a row in the html when a user adds an entry
+database.ref().on("child_added",function(childSnapShot,prevChildKey){
+    console.log(childSnapShot.val());
+    // Store everything in a variable
+    var trainName = childSnapShot.val().name;
+    var destination = childSnapShot.val().destination;
+    var trainTime = childSnapShot.val().time;
+    var trainFrequency = childSnapShot.val().frequency;
+
+    // Train info
+    console.log(trainName);
+    console.log(destination);
+    console.log(trainTime);
+    console.log(trainFrequency);
+
+    // Add each train's data into the table
+   $("#train-table").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" +
+  trainFrequency + "</td><td>" + trainTime + "</td><td>" + "</td></tr>");
+});
+
