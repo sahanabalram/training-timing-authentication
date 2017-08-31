9930bc8da612338cce977763ce78fb9fcf6547a2
Sahana Balram <sahana2008balram@gmail.com>
2017-08-31 08:26:32 -0700

    Remove google signin functions

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -24,55 +24,6 @@ function signOut() {
     });
 }
 
-function isUserEqual(googleUser, firebaseUser) {
-  if (firebaseUser) {
-    var providerData = firebaseUser.providerData;
-    for (var i = 0; i < providerData.length; i++) {
-      if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
-          providerData[i].uid === googleUser.getBasicProfile().getId()) {
-        // We don't need to reauth the Firebase connection.
-        return true;
-      }
-    }
-  }
-  return false;
-}
-
-function onSignIn(googleUser) {
-    console.log('Google Auth Response', googleUser);
-    // We need to register an Observer on Firebase Auth to make sure auth is initialized.
-    var unsubscribe = firebase.auth().onAuthStateChanged(function (firebaseUser) {
-        unsubscribe();
-        // Check if we are already signed-in Firebase with the correct user.
-        if (!isUserEqual(googleUser, firebaseUser)) {
-            // Build Firebase credential with the Google ID token.
-            var credential = firebase.auth.GoogleAuthProvider.credential(
-                googleUser.getAuthResponse().id_token);
-            // Sign in with credential from the Google user.
-            firebase.auth().signInWithCredential(credential).then(function (user){
-                console.log("User logged in");
-                $("#login-container").hide();
-                $("#main-container").show();
-            }).catch(function (error) {
-                // Handle Errors here.
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                // The email of the user's account used.
-                var email = error.email;
-                // The firebase.auth.AuthCredential type that was used.
-                var credential = error.credential;
-                // ...
-                $("#login-container").show();
-                $("#main-container").hide();
-            });
-        } else {
-            console.log('User already signed-in Firebase.');
-            $("#login-container").hide();
-            $("#main-container").show();
-        }
-    });
-}
-
 function authenticate() {
     var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider).then(function (result) {
